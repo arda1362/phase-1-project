50fb7703b947fc2e977a5db23017364fb21c5e8c
Ardavan <126632174+arda1362@users.noreply.github.com>
2023-07-22 22:55:06 +0000

    index.js

--- index.js
+++ index.js
@@ -1,137 +1,70 @@
-function performOperation() {
-  var operation = document.getElementById("operation").value;
-  var expression = encodeURIComponent(document.getElementById("expression").value);
-  var url = "https://newton.now.sh/api/v2/" + operation + "/" + expression;
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      var result = data.result;
-      document.getElementById("result").textContent = "Result: " + result;
-    })
-    .catch(error => {
-      console.log("Error:", error);
+// Function to fetch data from the Newton API
+function fetchData(operation, expression) {
+  const apiUrl = `https://newton.now.sh/api/v2/${operation}/${encodeURIComponent(expression)}`;
+  return fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => data.result)
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+      return null;
     });
 }
 
-// Callback function for document event listener
-function handleDocumentEvent() {
-  console.log('Document event listener triggered');
-  fetchData()
-    .then(result => {
-      const output = document.getElementById('result');
-      output.innerHTML = JSON.stringify(result, null, 2);
-    })
-    .catch(error => console.log(error));
+// Function to show loading state in the result div
+function showLoading() {
+  const resultDiv = document.getElementById("result");
+  resultDiv.innerHTML = "Loading...";
 }
 
-// Callback function for element event listener
-function handleElementEvent() {
-  console.log('Element event listener triggered');
-  fetchData()
-    .then(result => {
-      const output = document.getElementById('result');
-      output.innerHTML = JSON.stringify(result, null, 2);
-    })
-    .catch(error => console.log(error));
+// Function to display the result in the result div
+function displayResult(result) {
+  const resultDiv = document.getElementById("result");
+  if (result !== null) {
+    resultDiv.innerHTML = `<p><strong>Result:</strong> ${result}</p>`;
+  } else {
+    resultDiv.innerHTML = "Error occurred while fetching data.";
+  }
 }
 
-// Callback function for blur logo event listener
-function handleBlurLogoEvent() {
-  console.log('Blur logo event listener triggered');
-  fetchData()
-    .then(result => {
-      const output = document.getElementById('result');
-      output.innerHTML = JSON.stringify(result, null, 2);
-    })
-    .catch(error => console.log(error));
+// Function to perform the operation when the button is clicked
+function performOperation() {
+  const selectedOperation = document.getElementById("operation").value;
+  const expression = document.getElementById("expression").value;
+
+  // Show loading state while fetching data
+  showLoading();
+
+  fetchData(selectedOperation, expression)
+    .then(displayResult);
 }
 
 // Event listener for the document
-document.addEventListener('keydown', handleDocumentKeydown);
-document.addEventListener('keyup', handleDocumentKeyup);
-document.addEventListener('keypress', handleDocumentKeypress);
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the logo element
+  const logo = document.getElementById("logo");
 
-// Event listener for specific elements using querySelector
-const element = document.querySelector('#elementId');
-const htmlElement = document.querySelector('.htmlElementClass');
-const inputElement = document.querySelector('input[type="text"]');
+  // Add event listener for mouseleave to blur the logo
+  logo.addEventListener("mouseleave", function() {
+    this.style.filter = "blur(2px)";
+  });
 
-if (element) {
-  element.addEventListener('keydown', handleElementKeydown);
-  element.addEventListener('keyup', handleElementKeyup);
-  element.addEventListener('keypress', handleElementKeypress);
-}
+  // Event listeners for specific elements using querySelector
+  const operationSelect = document.querySelector('#operation');
+  const expressionInput = document.querySelector('#expression');
+  const button = document.querySelector('button');
 
-if (htmlElement) {
-  htmlElement.addEventListener('keydown', handleHTMLElementKeydown);
-  htmlElement.addEventListener('keyup', handleHTMLElementKeyup);
-  htmlElement.addEventListener('keypress', handleHTMLElementKeypress);
-}
+  // Add event listener for the button click to perform the operation
+  button.addEventListener('click', performOperation);
 
-if (inputElement) {
-  inputElement.addEventListener('keydown', handleHTMLInputElementKeydown);
-  inputElement.addEventListener('keyup', handleHTMLInputElementKeyup);
-  inputElement.addEventListener('keypress', handleHTMLInputElementKeypress);
-}
+  // Add event listener for the keypress event on the expression input
+  expressionInput.addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+      performOperation();
+    }
+  });
 
-// Event handler functions
-function handleDocumentKeydown(event) {
-  // Handle keydown event for the document
-  console.log('Document keydown event:', event.key);
-}
-
-function handleDocumentKeyup(event) {
-  // Handle keyup event for the document
-  console.log('Document keyup event:', event.key);
-}
-
-function handleDocumentKeypress(event) {
-  // Handle keypress event for the document
-  console.log('Document keypress event:', event.key);
-}
-
-function handleElementKeydown(event) {
-  // Handle keydown event for the targeted element
-  console.log('Element keydown event:', event.key);
-}
-
-function handleElementKeyup(event) {
-  // Handle keyup event for the targeted element
-  console.log('Element keyup event:', event.key);
-}
-
-function handleElementKeypress(event) {
-  // Handle keypress event for the targeted element
-  console.log('Element keypress event:', event.key);
-}
-
-function handleHTMLElementKeydown(event) {
-  // Handle keydown event for the targeted HTMLElement
-  console.log('HTMLElement keydown event:', event.key);
-}
-
-function handleHTMLElementKeyup(event) {
-  // Handle keyup event for the targeted HTMLElement
-  console.log('HTMLElement keyup event:', event.key);
-}
-
-function handleHTMLElementKeypress(event) {
-  // Handle keypress event for the targeted HTMLElement
-  console.log('HTMLElement keypress event:', event.key);
-}
-
-function handleHTMLInputElementKeydown(event) {
-  // Handle keydown event for the targeted HTMLInputElement
-  console.log('HTMLInputElement keydown event:', event.key);
-}
-
-function handleHTMLInputElementKeyup(event) {
-  // Handle keyup event for the targeted HTMLInputElement
-  console.log('HTMLInputElement keyup event:', event.key);
-}
-
-function handleHTMLInputElementKeypress(event) {
-  // Handle keypress event for the targeted HTMLInputElement
-  console.log('HTMLInputElement keypress event:', event.key);
-}
+  // Add event listener for mouseleave to blur the logo
+  logo.addEventListener('mouseleave', function() {
+    this.style.filter = 'blur(2px)';
+  });
+});
